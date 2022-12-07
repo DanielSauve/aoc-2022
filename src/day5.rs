a3f99e92ebfdd6a2974e746f7ec98ecfccdcf8c1
Daniel <danielsauve@protonmail.com>
2022-12-06 16:50:46 -0800

    Initial solution for Day 5 Part 2

--- src/day5.rs
+++ src/day5.rs
@@ -50,7 +50,53 @@ pub fn day5part1(input: &str) -> u32 {
 }
 
 pub fn day5part2(input: &str) -> u32 {
-    let lines = input.lines();
+    let mut lines = input.lines();
     let mut score = 0;
+    let mut stacks: Vec<VecDeque<&str>> = vec![];
+    for x in 0..9 {
+        stacks.push(VecDeque::new());
+    }
+    loop {
+        let line = lines.next().unwrap();
+        if line.is_empty() {
+            break;
+        }
+        let mut current = line.get(0..3).unwrap();
+        let mut i = 0usize;
+        let mut curr_char = 0usize;
+        loop {
+            let thing = current.get(1..2).unwrap();
+            let re = Regex::new(r"[A-Z]").unwrap();
+            if re.is_match(thing) {
+                stacks[i].push_front(thing);
+            }
+            i += 1;
+            curr_char += 4;
+            let next = line.get(curr_char..curr_char + 3);
+            if next.is_none() {
+                break;
+            }
+            current = next.unwrap();
+        }
+    }
+    for line in lines {
+        let mut split = line.split(' ');
+        let count: u32 = split.nth(1).unwrap().parse().unwrap();
+        let mut from: usize = split.nth(1).unwrap().parse().unwrap();
+        let mut to: usize = split.nth(1).unwrap().parse().unwrap();
+        to -= 1;
+        from -= 1;
+        let mut temp_stack: Vec<&str> = Vec::new();
+        for _ in 0..count {
+            temp_stack.push(stacks[from].pop_back().unwrap());
+        }
+        for _ in 0..count {
+            stacks[to].push_back(temp_stack.pop().unwrap());
+        }
+    }
+    for mut x in stacks {
+        print!("{}", x.pop_back().unwrap());
+    }
+    println!("");
     score
 }
